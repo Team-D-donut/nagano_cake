c92cefb8e07d337b4145ad52c7db68649e98ac2b
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-06-20 00:55:39 +0000

    fix-setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/intex'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -55,4 +12,43 @@ devise_for :admin,skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ scope module: :pabulic do
+ root to: 'homes#top'
+ get '/about', to: 'homes#about'
+ resources :items, only: %i[index show]
+ resources :customers, only: [] do
+ 
+ collection do
+ get 'show', to: 'customers#show', as: 'current_customer'
+ get 'information/edit', to: 'customers#edit', as: 'edit_current_customer'
+ patch 'update', to: 'customers#update', as: 'update_current_customer'
+ get 'confirm', to: 'customers#confirm', as: 'confirm_current_customer'
+ patch 'withdraw', to: 'customers#withdraw', as: 'withdraw_current_customer'
+ end
+end
+
+resources :cart_items, only: %i[index create destroy] do
+ collection do
+  delete :destroy_all
+ end
+end
+
+resources :orders, only: %i[new create index show] do
+  collection do
+    post :confirm
+    get :complete
+  end
+end
+resources :addresses, only: %i[index edit create update destroy]
+end
+
+
+namespace :admin do
+  root to: 'homes#top'
+  resources :items, only: %i[index new create show edit update]
+  resources :genres, only: %i[index create edit update]
+  resources :customers, only: %i[index show edit update]
+  resources :orders, only: %i[show update]
+  resources :ordered_items, only: %i[update]
+end
 end
