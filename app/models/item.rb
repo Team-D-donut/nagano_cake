72bc70a9c9725f624cb2df4ca4c77d5e18a64490
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-06-21 01:45:32 +0000

    [update]merge

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :ordered_items  #中間テーブル
   has_many :orders, through: :ordered_items
+  
+  def taxin_price
+        price*1.1
+  end
 end
