5dda926b8edd7fe08dbcb91f0ba117a6ca04818e
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-06-21 00:18:24 +0000

    [update]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,49 @@
 class Public::AddressesController < ApplicationController
+  def create
+    @address = Address.new(address_params)
+    @address.user_id = current_user.id
+      if @address.save
+        flash[:notice] = "You have created address successfully."
+      redirect_to addresses_path
+      #else
+      #@user = current_user
+      #@books = Book.all
+      #render :index
+      end
+  end
+  
   def index
+    @addresses = Address.all
+    @address= Address.new
+    @customer = current_customer
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    if @address.save
+      flash[:notice] = "You have updated book successfully."
+      redirect_to address_path(@address.id)
+    else
+      @addresses = Address.all
+      render :edit
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])  # データ（レコード）を1件取得
+    address.destroy  # データ（レコード）を削除
+    redirect_to '/addresses'  # 配送先一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  has_many :cart_items
+  has_many :ordered_items  #中間テーブル
+  has_many :orders, through: :ordered_items
 end
