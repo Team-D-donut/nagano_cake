f88373a1fa15b203d7544dbb9589987f83393ddd
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-06-21 00:59:20 +0000

    [add]admin/customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
