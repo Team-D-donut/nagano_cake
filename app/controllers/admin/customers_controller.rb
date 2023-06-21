bbc122659cee4dbf9598b91b183cc6d66d765e48
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-21 10:17:01 +0000

    [update]admin/customer index,show,edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,13 +8,19 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
-    # @customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def update
     @customer = Customer.find(params[:id])
-    @customer.update
-    redirect_to admin_customer_path(@customer)
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :post_code, :address, :telephone_number, :email, :is_deleted)
   end
   
 end
