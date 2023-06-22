fcf25f26b7af774ea361fa06b6d82b799c2e9b93
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-22 01:23:06 +0000

    [update]admin/login, admin/customer/index,edit

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -8,25 +8,8 @@ protected
     admin_root_path
   end
 
-  
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
 end
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-
-
-  # If you have extra params to permit, append them to the sanitizer.
-
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,14 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :kana_last_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
   validates :is_deleted, inclusion: [true, false]
 end
