0f9409c8a112c0e34f657efbf0500d0998224c30
asukaaasaaa <132123244+asukaaasaaa@users.noreply.github.com>
2023-06-23 02:37:59 +0000

    [add]enum order/ordered_item

--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -1,4 +1,7 @@
 class OrderedItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  # 製作ステータス 0=着手不可, 1=製作待ち, 2=製作中, 3=制作完了
+  enum make_status: { production_not_possible: 0, production_pending: 1, in_production: 2, production_complete: 3 }
 end
