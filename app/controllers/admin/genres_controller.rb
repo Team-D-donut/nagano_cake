a38211fd37e8532369a536d2e7e05ffb6b324b4e
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-26 00:53:06 +0000

    [update]admin/login, error_message

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,9 +1,11 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @genres = Genre.all
     @genre = Genre.new
   end
-  
+
   def create
     @genre = Genre.new(genre_params)
    if @genre.save
@@ -17,7 +19,7 @@ class Admin::GenresController < ApplicationController
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
     @genre = Genre.find(params[:id])
    if  @genre.update(genre_params)
@@ -26,7 +28,7 @@ class Admin::GenresController < ApplicationController
     render "edit"
    end
   end
-  
+
   private
   def genre_params
     params.require(:genre).permit(:name)
