2939e89a0349c0a61f20f2764470cdc1c57ba8c3
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-06-26 09:49:33 +0000

    [Fix]cart_index for destroy_all

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,10 @@
 class Public::CartItemsController < ApplicationController
-  
+
   def index
     @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
-  
+
   def create
         @cart_item = current_customer.cart_items.new(cart_item_params)
         # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
@@ -14,7 +14,7 @@ class Public::CartItemsController < ApplicationController
             cart_item.save
             redirect_to cart_items_path
 
-        # もしカート内に「同じ」商品がない場合は通常の保存処理 
+        # もしカート内に「同じ」商品がない場合は通常の保存処理
         elsif @cart_item.save
               @cart_items = current_customer.cart_items.all
             render 'index'
@@ -28,7 +28,7 @@ class Public::CartItemsController < ApplicationController
         @cart_items = CartItem.all
         render 'index'
   end
-        
+
   def destroy
         cart_item = CartItem.find(params[:id])
         cart_item.destroy
@@ -37,16 +37,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all  #カート内全て削除
-        cart_items = CartItem.all
-        cart_items.destroy_all
+        @cart_items = CartItem.all
+        @cart_items.destroy_all
         render 'index'
   end
 
 
-  
+
   private
-  
+
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :price, :quantity)
   end
-end 
+end
